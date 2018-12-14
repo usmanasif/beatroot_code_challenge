00d27aa763538f7efdbd1f0b707567446d5448e0
Usman Asif <usmanasif@users.noreply.github.com>
2018-12-14 19:22:27 +0500

    ReleasesController: Fetched releases from API and Listed them

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,5 +1,15 @@
 class ReleasesController < ApplicationController
   def index
+    response = HTTParty.get(
+      "#{ENV['API_BASE_URL']}/accounts/#{ENV['API_ACCOUNT_SLUG']}/releases",
+      headers: { Authorization: "Token token=#{ENV['API_TOKEN']}" }
+    )
+
+    @releases = if response.success? && response['releases'].present?
+      response['releases']
+    else
+      []
+    end
   end
 
   def download
